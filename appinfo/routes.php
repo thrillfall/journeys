b4ab32838d1a6b8920864fcf68149c1fc8f3be1e
thrillfall <thrillfall@disroot.org>
2025-07-13 14:11:22 +0200

    this is already in context of the app. adding the full path breaks it

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -3,12 +3,12 @@ return [
     'routes' => [
         [
             'name' => 'personal_settings#startClustering',
-            'url' => '/apps/journeys/personal_settings/start_clustering',
+            'url' => '/personal_settings/start_clustering',
             'verb' => 'POST',
         ],
         [
             'name' => 'personal_settings#lastRun',
-            'url' => '/apps/journeys/personal_settings/last_run',
+            'url' => '/personal_settings/last_run',
             'verb' => 'GET',
         ],
     ],
